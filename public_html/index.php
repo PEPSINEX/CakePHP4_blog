4ff26034d579af71cadacfb338cf1c629cc1905e
PEPSINEX <kagakufact@yahoo.co.jp>
2021-05-16 23:29:19 +0900

    [update] フォルダ構成変更に伴うCakePHPの設定変更

--- public_html/index.php
+++ public_html/index.php
@@ -1,22 +1,6 @@
 <?php
-/**
- * The Front Controller for handling every request
- *
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.2.9
- * @license       MIT License (https://opensource.org/licenses/mit-license.php)
- */
-
 // Check platform requirements
-require dirname(__DIR__) . '/config/requirements.php';
+require dirname(__DIR__) . '/cakephp/config/requirements.php';
 
 // For built-in server
 if (PHP_SAPI === 'cli-server') {
@@ -28,13 +12,13 @@ if (PHP_SAPI === 'cli-server') {
         return false;
     }
 }
-require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . '/cakephp/vendor/autoload.php';
 
 use App\Application;
 use Cake\Http\Server;
 
 // Bind your application to the server.
-$server = new Server(new Application(dirname(__DIR__) . '/config'));
+$server = new Server(new Application(dirname(__DIR__) . '/cakephp/config'));
 
 // Run the request/response through the application and emit the response.
 $server->emit($server->run());
